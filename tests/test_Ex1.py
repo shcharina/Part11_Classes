da1464b90fc28b7bf2e85a6379d1b3c676c07364
shcharina <arina.shchelkanova@gmail.com>
2025-03-20 13:31:48 +0700

    new file:   tests/__pycache__/test_Ex1.cpython-313-pytest-8.3.5.pyc 	modified:   tests/test_Ex1.py

--- tests/test_Ex1.py
+++ tests/test_Ex1.py
@@ -2,8 +2,6 @@ import pytest
 
 from src.Ex1 import Friends
 
-friends = Friends({"a", "b"}, {"b", "c"}, {"c", "a"}, {"a", "c"})
-
 @pytest.mark.parametrize(
     'connection, is_new_connection',
     [
@@ -12,6 +10,7 @@ friends = Friends({"a", "b"}, {"b", "c"}, {"c", "a"}, {"a", "c"})
     ]
 )
 def test_add(connection, is_new_connection):
+    friends = Friends({"a", "b"}, {"b", "c"}, {"c", "a"}, {"a", "c"})
     # Arrange & Act
     is_new_conn = friends.add(connection=connection)
 
@@ -21,13 +20,46 @@ def test_add(connection, is_new_connection):
 @pytest.mark.parametrize(
     'connection, is_existing_connection',
     [
-        ({"a", "f"}, False),
-        ({"a", "b"}, False)
+        ({"a", "f"}, True),
+        ({"a", "b"}, True),
+        ({"a", "z"}, False)
     ]
 )
 def test_remove(connection, is_existing_connection):
+    friends = Friends({"a", "b"}, {"b", "c"}, {"c", "a"}, {"a", "c"}, {"a", "f"})
+
     # Arrange & Act
-    is_new_conn = friends.add(connection=connection)
+    is_existing_conn = friends.remove(connection=connection)
 
     # Assert
-    assert is_new_conn == is_existing_connection
+    assert is_existing_conn == is_existing_connection
+
+@pytest.mark.parametrize(
+    'connections, expected_names_of_their_friends',
+    [
+        (({"a", "b"}, {"b", "c"}, {"c", "d"}), {"a", "b", "c", "d"}),
+        (({"a", "b"}, {"b", "c"}), {"a", "b", "c"})
+    ]
+)
+def test_names(connections, expected_names_of_their_friends):
+    friends = Friends(*connections)
+    # Arrange & Act
+    names_of_their_friends = friends.names()
+
+    # Assert
+    assert names_of_their_friends == expected_names_of_their_friends
+
+@pytest.mark.parametrize(
+    'name, their_expected_friends',
+    [
+        ("a", {"b", "c"}),
+        ("d", set())
+    ]
+)
+def test_connected(name, their_expected_friends):
+    friends = Friends({"a", "b"}, {"b", "c"}, {"c", "a"})
+    # Arrange & Act
+    their_friends = friends.connected(name)
+
+    # Assert
+    assert their_friends == their_expected_friends
